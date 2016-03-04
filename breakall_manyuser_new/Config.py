513421583565413ccc64dfbaebe2e8485ba92d3f
superfls <lincappu@163.com>
2016-03-04 12:48:34 +0800

    test

---
+++ breakall_manyuser_new/Config.py
@@ -0,0 +1,12 @@
+﻿#Config
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = 3306
+MYSQL_USER = 'wolflink'
+MYSQL_PASS = 'wolflink2015'
+MYSQL_DB = 'shadowsocks'
+
+MANAGE_PASS = 'wolflink2015'
+#if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+#make sure this port is idle
+MANAGE_PORT = 23333
