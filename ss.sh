19ec44a2dca00de8425080a8a023b2158bea05e4
superfls <lincappu@163.com>
2016-03-04 13:16:56 +0800

    fix ss.sh

--- ss.sh
+++ ss.sh
@@ -3,13 +3,21 @@ bash /root/denyiplist.sh
 echo "4.denyiplist  SUCCESS"
 bash /root/inset_ss.sh
 echo "5.insert_ss SUCCESS"
-nohup python server.py   >> /var/log/shadowsocks.log   2>&1    &
+nohup python /root/breakall_manyuser_new/server.py   >> /var/log/shadowsocks.log   2>&1    &
 echo  "6.start SS  SUCCESS"
 cp -f /root/supervisord.conf /etc/supervisord.conf
-echo  "6.autostart  SUCCESS"
+echo  "7.autostart  SUCCESS"
 cp  -f /root/sysctl.conf /etc/sysctl.conf
 systemctl start  supervisord.service
 systemctl enable  supervisord.service
-
+echo 
+touch /root/netstat 
+netstat -tunlp > /root/netstat
+tail -n 10 /root/netstat
+echo 
+tail -n 10 /var/log/shadowsocks.log
+echo 
+ps -aux | grep python
+PID=$(ps -A |  grep 'python'| awk '{print $1}')
 
 
