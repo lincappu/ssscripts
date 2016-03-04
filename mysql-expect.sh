1e9ad72671c8926d4e2fe3e9b6562577de3fcea7
superfls <lincappu@163.com>
2016-03-04 15:34:26 +0800

    fix file

--- mysql-expect.sh
+++ mysql-expect.sh
@@ -2,6 +2,7 @@
 yum -y install mariadb-server mariadb
 systemctl start mariadb.service
 systemctl enable mariadb.service
+chmod +x /root/mysql-expect.exp
 ./mysql-expect.exp
 echo
 echo "2.mariadb  install SUCCESS"
