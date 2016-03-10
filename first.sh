427773f45e34fb5805f3f7668caf7f91645cfac7
superfls <lincappu@163.com>
2016-03-10 13:31:09 +0800

    fix fle

--- first.sh
+++ first.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ ! -f "/root/ss.tar.gz" ]; then
+if [ ! -f "/root/ssscripts.tar.gz" ]; then
         echo "this server no ss-file.please check from  source server!"
         exit 0 
 fi 
-tar -zxvf /root/ss.tar.gz
+tar -zxvf /root/ssscripts.tar.gz
 bash /root/softs.sh
 bash  /root/mysql-expect.sh
 bash  /root/mysql_sql.sh
