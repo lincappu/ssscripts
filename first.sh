1eb3ddab883725917ca808ba939cc651961482c4
superfls <superfls@users.noreply.github.com>
2016-03-10 11:09:24 +0800

    Update first.sh

--- first.sh
+++ first.sh
@@ -8,13 +8,4 @@ bash /root/softs.sh
 bash  /root/mysql-expect.sh
 bash  /root/mysql_sql.sh
 bash /root/ss.sh
-#!/bin/bash
-if [ ! -f "/root/ssscripts.tar.gz" ]; then
-        echo "this server no ss-file.please check from  source server!"
-        exit 0 
-fi 
-tar -zxvf /root/ssscripts.tar.gz
-bash /root/softs.sh
-bash  /root/mysql-expect.sh
-bash  /root/mysql_sql.sh
-bash /root/ss.sh
+
